db86c88a84e32dba18672b49f200d5e9f71edf12
Max74112 <136378206+Max74112@users.noreply.github.com>
2023-11-30 14:24:14 +0800

    移除HTML

--- app/CrawlList.js
+++ app/CrawlList.js
@@ -49,7 +49,7 @@ let ParseTable = async (outputArray = [], baseURL) => {
     // @TODO 3. 取得ID
     // 請修改此處以抓取正確的資訊。
     output['dc.title'] = eleTr.find('td:eq(1)').html()
-    output['dc.title'] = StripHTML(output['dc.title'])
+    
 
     let itemURL = eleTr.find('td a[href]').attr('href')
     itemURL = ResolveFullURL(baseURL, itemURL)
@@ -63,8 +63,7 @@ let ParseTable = async (outputArray = [], baseURL) => {
 
     // 將問題儲存到dc.title
     output['dc.title'] = eleTr.find('td:eq(1)').html()
-    let date = eleTr.find('td:eq(2)').html()
-
+    output['dc.title'] = StripHTML(output['dc.title'])
     // 將日期儲存到dc.date
     output['dc.date'] = ResolveDate(date)
 
